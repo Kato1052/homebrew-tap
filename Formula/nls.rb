edfe0d01e61f511296350608ac108689812ca934
Kato <kato.mizuki.ksu@gmail.com>
2026-07-15 13:29:37 +0900

    Fix manually indicate version for nls

--- Formula/nls.rb
+++ Formula/nls.rb
@@ -5,25 +5,24 @@ class Nls < Formula
   desc "ls without month abbreviation."
   homepage "https://github.com/Kato1052/nls"
   # url "https://github.com/Kato1052/nls/archive/refs/tags/v0.0.8.tar.gz"
-  # version "0.0.8"
+  version "0.0.8"
   # sha256 "87a89d4029f3f87d11dee838262e59853391c72825290eb0536936c4dcb72eee"
   license "MIT"
-  version VERSION
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Kato1052/nls/releases/download/v#{VERSION}/nls-#{VERSION}_arm64_darwin.tar.gz"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_arm64_darwin.tar.gz"
     sha256 "c08a4b28413f7b8f757baded90b87b2620acdf4617e18f18f800a6a09b521580"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/Kato1052/nls/releases/download/v#{VERSION}/nls-#{VERSION}_arm64_linux.tar.gz"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_arm64_linux.tar.gz"
     sha256 "a11d0014fe549d113c5e9fe018671511f5be05c1b69f458cc1990eca003659c9"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Kato1052/nls/releases/download/v#{VERSION}/nls-#{VERSION}_amd64_darwin.tar.gz"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_amd64_darwin.tar.gz"
     sha256 "2414df8d8eb4d2db9cb6f341da5ef1dddff236c335cadfc57d7aa152d83c687b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Kato1052/nls/releases/download/v#{VERSION}/nls-#{VERSION}_amd64_linux.tar.gz"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_amd64_linux.tar.gz"
     sha256 "f1e4fc9b0721f8a1c9aafe8605ff2aee7e9b550f1cf3fcf4ca95b0e55dc8003c"
   end
 
