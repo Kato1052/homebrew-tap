8584fdcd313167637f8d82c1c81b423485999757
Kato <kato.mizuki.ksu@gmail.com>
2026-07-15 13:02:00 +0900

    create .gitignore and nls.rb

---
+++ Formula/nls.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Nls < Formula
+  desc "ls without month abbreviation."
+  homepage "https://github.com/Kato1052/nls"
+  url "https://github.com/Kato1052/nls/archive/refs/tags/v0.0.8.tar.gz"
+  version "0.0.8"
+  sha256 "87a89d4029f3f87d11dee838262e59853391c72825290eb0536936c4dcb72eee"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test nls`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
