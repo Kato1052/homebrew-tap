480f61b1f1d73723311555199dd2196092309a79
Kato <kato.mizuki.ksu@gmail.com>
2026-07-15 14:13:50 +0900

    nls: update to 0.0.9

--- Formula/nls.rb
+++ Formula/nls.rb
@@ -4,41 +4,27 @@
 class Nls < Formula
   desc "ls without month abbreviation."
   homepage "https://github.com/Kato1052/nls"
-  # url "https://github.com/Kato1052/nls/archive/refs/tags/v0.0.8.tar.gz"
-  version "0.0.8"
-  # sha256 "87a89d4029f3f87d11dee838262e59853391c72825290eb0536936c4dcb72eee"
+  version "0.0.9"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_arm64_darwin.tar.gz"
-    sha256 "c08a4b28413f7b8f757baded90b87b2620acdf4617e18f18f800a6a09b521580"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.9/nls-0.0.9_arm64_darwin.tar.gz"
+    sha256 "17ce4821301967f8974505cd701c86ab9136bb955b5915b2e8285861ac44751d"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_arm64_linux.tar.gz"
-    sha256 "a11d0014fe549d113c5e9fe018671511f5be05c1b69f458cc1990eca003659c9"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.9/nls-0.0.9_arm64_linux.tar.gz"
+    sha256 "0ba4cd0535b726ca7b8028f52b4587f60373925d3f383a13d18afbb2bc9e0002"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_amd64_darwin.tar.gz"
-    sha256 "2414df8d8eb4d2db9cb6f341da5ef1dddff236c335cadfc57d7aa152d83c687b"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.9/nls-0.0.9_amd64_darwin.tar.gz"
+    sha256 "e2276e91ea2b09a981687afdc6fe0b72acd15b9d22474c31c7156b7de672ff1f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Kato1052/nls/releases/download/v0.0.8/nls-0.0.8_amd64_linux.tar.gz"
-    sha256 "f1e4fc9b0721f8a1c9aafe8605ff2aee7e9b550f1cf3fcf4ca95b0e55dc8003c"
+    url "https://github.com/Kato1052/nls/releases/download/v0.0.9/nls-0.0.9_amd64_linux.tar.gz"
+    sha256 "53016fc664b2aae0d45b09fe21ca5ee141fc6efe32b7d059e7a38fef9f5b9eaf"
   end
 
-  # depends_on "cmake" => :build
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
-
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
-    # system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     bin.install "nls"
 
     zsh_completion.install "completions/zsh/_nls"
@@ -47,16 +33,6 @@ class Nls < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test nls`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    # system "false"
     system "#{bin}/nls --help"
   end
 end
